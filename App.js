df402c693ac4c294ea72f2fc052bdd7006f9fc76
Pedro Henrique Marinho Durek <pedro_hmd@hotmail.com>
2018-03-02 23:01:14 -0300

    feat: TabNavigator and Decks included

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Platform } from 'react-native'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import reducer from './reducers'
 import CardsStatusBar from './components/CardsStatusBar'
+import Decks from './views/Decks'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+const Tabs = TabNavigator(
+    {
+        Decks: {
+            screen: Decks,
+            navigationOptions: {
+                tabBarLabel: 'Decks',
+                tabBarIcon: ({ tintColor }) => <Icon name={"glass"} size={30} color={tintColor} />
+            }
+        }
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: '#000',
+            style: {
+                backgroundColor: '#fff'
+            }
+        }
+    }
+)
+
+const MainNavigator = StackNavigator({
+    Home: {
+        screen: Tabs
+    }
+})
 
 class App extends Component {
     
     store = createStore(reducer)
-    
+
     render() {
-        return (
-            <Provider store={this.store}>
-                <View>
-                    
+        return <Provider store={this.store}>
+                <View style={{ flex: 1 }}>
+                    <MainNavigator />
                 </View>
             </Provider>
-        )
     }
 }
 
---
+++ views/Decks.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Decks extends Component {
+    render() {
+        return <View />
+    }
+}
+
+export default Decks
