5dabd2028e30fe0348a0cb768c1cd755fc313908
Pedro Henrique Marinho Durek <pedro_hmd@hotmail.com>
2018-03-02 22:05:30 -0300

    feat: Insert redux to the project

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native'
+import reducer from './reducers'
+import CardsStatusBar from './components/CardsStatusBar'
 
 class App extends Component {
+    
+    store = createStore(reducer)
+    
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
-            </View>
+            <Provider store={this.store}>
+                <View>
+                    
+                </View>
+            </Provider>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
 
 export default App
