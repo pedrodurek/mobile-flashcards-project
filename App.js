62521c9eff9a1907094c9c4a73c1cbdf537ac225
Pedro Henrique Marinho Durek <pedro_hmd@hotmail.com>
2018-03-30 23:05:22 -0300

    refact: splitted routes from application entry point

--- App.js
+++ App.js
@@ -2,36 +2,9 @@ import React, { Component } from 'react'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native'
-import { StackNavigator } from 'react-navigation'
 import thunk from 'redux-thunk'
-import Decks from '@screens/Decks'
-import DeckView from '@screens/DeckView'
-import AddEditCard from '@screens/AddEditCard'
-import NewEditDeck from '@screens/NewEditDeck'
-import Quiz from '@screens/Quiz'
-import Home from '@screens/Home'
 import reducer from '@reducers'
-
-const MainNavigator = StackNavigator({
-    Home: {
-        screen: Home
-    },
-    Decks: {
-        screen: Decks
-    },
-    NewEditDeck: {
-        screen: NewEditDeck
-    },
-    DeckView: {
-        screen: DeckView
-    },
-    AddEditCard: {
-        screen: AddEditCard
-    },
-    Quiz: {
-        screen: Quiz
-    }
-})
+import { MainNavigator } from '@routes'
 
 class App extends Component {
     render() {
